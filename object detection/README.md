0a201ae6c16455379ff4cc4fe7d9f0d6c7bc357b
Jeevanantham P <87052326+Jeevananthamcse@users.noreply.github.com>
2021-07-08 08:14:41 +0530

    Add files via upload
    commit

---
+++ object detection/README.md
@@ -0,0 +1,17 @@
+## Real-Time-Object-Detection-using-Deep-Learning-and-Python.
+### Detect upto 50 objects in real time using pre-trained deep learning models along with OpenCV and Python
+
+#### * This algorithm can detect the following objects in real time
+ 	["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+
+
+### Object Detection 1
+![](Screenshot%20from%202019-09-15%2013-07-26.png)
+
+
+### Object Detection 2
+![](Screenshot%20from%202019-09-15%2013-08-59.png)
+
---
+++ object detection/detect.py
@@ -0,0 +1,92 @@
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import imutils
+import time
+import cv2
+
+#initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor","pen"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt',
+                               'MobileNetSSD_deploy.caffemodel')
+ 
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
+
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	# loop over the detections
+	for i in np.arange(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence > 0.2:
+			# extract the index
+			idx = int(detections[0, 0, i, 1])
+             #then compute the (x, y)-coordinates of  the bounding box for the object
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# draw the prediction on the frame
+			label = "{}".format(CLASSES[idx],
+				confidence * 100)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+
+			import pyttsx3
+			engine = pyttsx3.init()
+
+			engine.say(label)
+			engine.say("is in front of you")
+			engine.runAndWait()
+
+	# show the output frame
+	cv2.imshow("Frame", imutils.resize(frame,width=500))
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
